d224069cd09a8306912ecee03886ba352c35af76
Vojtech Barticka <bartickavojtech@gmail.com>
2020-09-23 11:28:25 +0200

    minor changes, file appending and link depth added

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
 class Const:
-    chrome_path = "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+    chromedriver_path = "C:/Users/Vojtěch Bartička/Desktop/BP/chromedriver.exe"
     link_keywords = ["podminky", "smluvni", "ochrana", "soukromi", "cookie", "uzivani", "osobni", "terms", "conditions",
                      "policy", "privacy", "udaju", "udaje"]
 
--- main.py
+++ main.py
@@ -3,61 +3,75 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from const import Const
 
-# pro hledani odkazu v html
+# pro operace s html
 from bs4 import BeautifulSoup
 
 import time
+
 
 # inicializace selenia
 def main():
     chrome_options = Options()
     chrome_options.add_argument("--headless")
-    chrome_exec_path = "C:/Program Files (x86)/Google/Chrome/Application/chromedriver.exe"
-    driver = webdriver.Chrome(executable_path=chrome_exec_path, options=chrome_options)
+    driver = webdriver.Chrome(executable_path=Const.chromedriver_path, options=chrome_options)
     url = "https://o.seznam.cz/ochrana-udaju/"
-    max_depth = 1
+    max_depth = 2
     scrape_page(driver, url, max_depth)
 
 
 # zakladni mechanismus nacteni cele stranky a jejiho stazeni
-def scrape_page(driver, url, max_depth, current_depth=0):
+def scrape_page(driver, url, max_depth, current_depth=0, append_file_path=None):
     driver.get(url)
     current_depth += 1
 
     last_height = driver.execute_script("return document.body.scrollHeight")
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(0.2)
+        time.sleep(1)
         new_height = driver.execute_script("return document.body.scrollHeight")
         if new_height == last_height:
             break
         last_height = new_height
 
     htmltext = driver.page_source
-    file_name = "".join(c for c in url if c.isalnum())
-    file = open(file_name + ".txt", "w", encoding="utf-8")
-    file.write(htmltext)
 
-    soup = BeautifulSoup(htmltext)
-    links = soup.find_all('a')
-    links = filter_links(links)
+    if append_file_path is None:
+        append_file_path = "".join(c for c in url if c.isalnum())
+        file = open(append_file_path + ".txt", "w", encoding="utf-8")
+        file.write("URL: " + url + "\n")
+        file.write(htmltext)
+        file.close()
+    else:
+        file = open(append_file_path + ".txt", "a", encoding="utf-8")
+        file.write("\nURL: " + url + "\n")
+        file.write(htmltext)
+        file.close()
 
     if current_depth == max_depth:
         return
     else:
+        soup = BeautifulSoup(htmltext)
+        links = soup.find_all('a')
+        links = filter_links(links, url)
+
+        scraped_urls = []
+        for link in links:
+            if link.get("href") not in scraped_urls:
+                scrape_page(driver, link.get("href"), max_depth, current_depth, append_file_path)
+                scraped_urls.append(link.get("href"))
         return
 
 
 # provadi filtrovani odkazu na zaklade klicovych slov
-def filter_links(links) -> list:
+def filter_links(links, current_url) -> list:
     relevant_links = []
     for link in links:
-        if "mailto" in link.get("href"):
+        if "mailto" in link.get("href") or current_url in link.get("href"):
             continue
         if any(keyword in link.get("href") for keyword in Const.link_keywords) or any(keyword in link.contents[0] for keyword in Const.link_keywords):
             relevant_links.append(link)
 
-    return links
+    return relevant_links
 
 
 main()
